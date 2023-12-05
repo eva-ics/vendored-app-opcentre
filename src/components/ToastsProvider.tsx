1b26cde255d7b1c46b08ace4910c983c8dec4316
Serhij S <div@altertech.com>
2023-12-05 01:37:07 +0100

    toast size

--- src/components/ToastsProvider.tsx
+++ src/components/ToastsProvider.tsx
@@ -1,7 +1,17 @@
 import { Toaster } from "react-hot-toast";
 
 const ToasterProvider = () => {
-  return <Toaster position={"bottom-left"} />;
+  return (
+    <Toaster
+      position={"bottom-left"}
+      toastOptions={{
+        style: {
+          maxWidth: "100%",
+          wordBreak: "break-all"
+        }
+      }}
+    />
+  );
 };
 
 export default ToasterProvider;
