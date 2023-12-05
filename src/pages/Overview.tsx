33598971af78047a0115843a770dc02bd11b1b46
RMerkulov <rmerkylov@altertech.com>
2023-12-05 13:15:03 +0200

    change section about

--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -81,7 +81,7 @@ const DashboardOverview = () => {
                 </table>
               </div>
             </div>
-            <div className="bmat-dashtable-container">
+            <div className="bmat-dashtable-container desktop-version">
               <div className="bmat-dashtable-title">About the application</div>
               <div className="bmat-dashtable-container-inner">
                 <div className="text-overview text-about">
@@ -145,6 +145,43 @@ const DashboardOverview = () => {
               );
             })}
           </div>
+          <div className="bmat-dashtable-container mobile-version"  >
+              <div className="bmat-dashtable-title">About the application</div>
+              <div className="bmat-dashtable-container-inner">
+                <div className="text-overview text-about">
+                  <img src="i/face_transparent.png" className="image-about" />
+                  <ul className="about-list">
+                  <li>
+                    EVA ICS operation centre is a vendored web application which
+                    allows operators to create custom dashboards, perform
+                    typical monitoring and analytics tasks.
+                  </li>
+                  <li>
+                    More information, help and tutorials can be found at the
+                    application page in{" "}
+                    <a href="https://info.bma.ai/en/actual/eva4/va/opcentre.html">
+                      Bohemia Automation Information System
+                    </a>
+                    .
+                    </li>
+                    </ul>
+                </div>
+               
+              </div>
+               <div className="text-overview text-about">
+                  <p>
+                    &copy;{" "}
+                    <a href="https://www.bohemia-automation.com/">
+                      Bohemia Automation Limited
+                    </a>
+                    . All rights reserved. The application is available under{" "}
+                    <a href="https://info.bma.ai/en/actual/eva4/license.html">
+                      EVA ICS license
+                    </a>{" "}
+                    with no restrictions for both personal and commercial usage.
+                  </p>
+                </div>
+            </div>
         </div>
       </div>
     </div>
