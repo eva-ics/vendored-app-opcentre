afca709309f504bd501d21d1237cd3a7f247e26b
Serhij S <div@altertech.com>
2023-12-13 01:11:04 +0100

    fix clicks on submenu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,105 +6,118 @@ import { NavLink } from "react-router-dom";
 import { useState } from "react";
 
 const Header = ({ toggleMenu, nav, logout, current_page }: HeaderProps) => {
-    const [openSubMenu, setOpenSubMenu] = useState<string | null>(null);
+	const [openSubMenu, setOpenSubMenu] = useState<string | null>(null);
 
-    const eva = get_engine() as Eva;
+	const eva = get_engine() as Eva;
 
-    return (
-        <header className="header">
-            <button className="menu-icon-btn" onClick={toggleMenu}>
-                <GiHamburgerMenu size={25} />
-            </button>
-            <div className="header-info">
-                <div className="dash-info">
-                    <img src="icon.svg" className="dash-logo" />
-                    <div className="dash-title">
-                        EVA ICS OpCentre. Node: {eva.system_name()} [
-                        {eva?.server_info?.aci.u}]
-                    </div>
-                </div>
-            </div>
-            <nav id="header">
-                <ul>
-                    {nav.map((v, idx) => {
-                        const isCurrent = current_page === v.value;
+	const handleSubClick = (event: any) => {
+		if (event.target.tagName !== "A") {
+			const link = event.currentTarget.querySelector("a");
+			if (link) {
+				link.click();
+			}
+		}
+	};
 
-                        const navLinkClass = isCurrent
-                            ? "nav-link nav-link-current"
-                            : "nav-link";
+	return (
+		<header className="header">
+			<button className="menu-icon-btn" onClick={toggleMenu}>
+				<GiHamburgerMenu size={25} />
+			</button>
+			<div className="header-info">
+				<div className="dash-info">
+					<img src="icon.svg" className="dash-logo" />
+					<div className="dash-title">
+						EVA ICS OpCentre. Node: {eva.system_name()} [
+						{eva?.server_info?.aci.u}]
+					</div>
+				</div>
+			</div>
+			<nav id="header">
+				<ul>
+					{nav.map((v, idx) => {
+						const isCurrent = current_page === v.value;
 
-                        const containerClass = isCurrent
-                            ? "nav-link-container nav-link-container-current"
-                            : "nav-link-container";
+						const navLinkClass = isCurrent
+							? "nav-link nav-link-current"
+							: "nav-link";
 
-                        return (
-                            <li
-                                className={navLinkClass}
-                                key={idx}
-                                onClick={() => {
-                                    if (
-                                        v.submenus &&
-                                        v.submenus.length > 0 &&
-                                        openSubMenu !== v.value
-                                    ) {
-                                        setOpenSubMenu(v.value);
-                                    } else {
-                                        setOpenSubMenu(null);
-                                        if (v.to?.startsWith("/")) {
-                                            document.location = v.to;
-                                        }
-                                    }
-                                }}
-                            >
-                                {v.to ? (
-                                    <NavLink key={idx} to={v.to}>
-                                        <div className={containerClass}>{v.value}</div>
-                                    </NavLink>
-                                ) : (
-                                    <div className={containerClass}>{v.value}</div>
-                                )}
+						const containerClass = isCurrent
+							? "nav-link-container nav-link-container-current"
+							: "nav-link-container";
 
-                                {openSubMenu == v.value &&
-                                    v.submenus &&
-                                    v.submenus.length > 0 && (
-                                        <ul className="submenu">
-                                            {v.submenus.map((submenuItem, subIdx) => (
-                                                <li className="submenu-item" key={subIdx}>
-                                                    <NavLink
-                                                        to={
-                                                            submenuItem.to === "logout"
-                                                                ? "?"
-                                                                : submenuItem.to
-                                                        }
-                                                        onClick={() => {
-                                                            if (
-                                                                submenuItem.to.startsWith(
-                                                                    "/"
-                                                                )
-                                                            ) {
-                                                                document.location =
-                                                                    submenuItem.to;
-                                                            } else if (
-                                                                submenuItem.to ===
-                                                                "logout"
-                                                            ) {
-                                                                logout();
-                                                            }
-                                                        }}
-                                                    >
-                                                        {submenuItem.value}
-                                                    </NavLink>
-                                                </li>
-                                            ))}
-                                        </ul>
-                                    )}
-                            </li>
-                        );
-                    })}
-                </ul>
-            </nav>
-        </header>
-    );
+						return (
+							<li
+								className={navLinkClass}
+								key={idx}
+								onClick={() => {
+									if (
+										v.submenus &&
+										v.submenus.length > 0 &&
+										openSubMenu !== v.value
+									) {
+										setOpenSubMenu(v.value);
+									} else {
+										setOpenSubMenu(null);
+										if (v.to?.startsWith("/")) {
+											document.location = v.to;
+										}
+									}
+								}}
+							>
+								{v.to ? (
+									<NavLink key={idx} to={v.to}>
+										<div className={containerClass}>{v.value}</div>
+									</NavLink>
+								) : (
+									<div className={containerClass}>{v.value}</div>
+								)}
+
+								{openSubMenu == v.value &&
+									v.submenus &&
+									v.submenus.length > 0 && (
+										<ul className="submenu">
+											{v.submenus.map((submenuItem, subIdx) => (
+												<li
+													className="submenu-item"
+													key={subIdx}
+													onClick={handleSubClick}
+												>
+													<NavLink
+														to={
+															submenuItem.to === "logout"
+																? "?"
+																: submenuItem.to
+														}
+														onClick={() => {
+															if (
+																submenuItem.to.startsWith(
+																	"/"
+																)
+															) {
+																document.location =
+																	submenuItem.to;
+															} else if (
+																submenuItem.to ===
+																"logout"
+															) {
+																logout();
+															}
+														}}
+													>
+														{submenuItem.value}
+													</NavLink>
+												</li>
+											))}
+										</ul>
+									)}
+							</li>
+						);
+					})}
+				</ul>
+			</nav>
+		</header>
+	);
 };
 
 export default Header;
