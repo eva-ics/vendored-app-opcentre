7930d496fefb5ed23619cd673cdac71669d1d643
RMerkulov <99145947+RoRomario360@users.noreply.github.com>
2024-01-04 10:58:21 +0200

    added blocks

---
+++ src/components/editors/profile_string.tsx
@@ -0,0 +1,31 @@
+import { TextField } from "@mui/material";
+import { useState } from "react";
+
+const ProfileEditor = ({
+    type,
+    current_value,
+    label,
+}: {
+    type: string;
+    current_value: string;
+    label?: string;
+}) => {
+    const [input_value, setInputValue] = useState(current_value);
+
+    return (
+        <div className="profile-input-wrapper">
+            <p className="page-label">{label}</p>
+            <TextField
+                fullWidth
+                type={type}
+                value={input_value}
+                onChange={(e) => {
+                    const val = e.target.value;
+                    setInputValue(val);
+                }}
+            />
+        </div>
+    );
+};
+
+export default ProfileEditor;
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,13 +1,35 @@
+import { Button, styled } from "@mui/material";
+import ProfileEditor from "../components/editors/profile_string";
+
+//mui styles
+const ButtonProfile = styled(Button)({
+    color: "gray",
+    borderColor: "gray",
+    "&:hover": {
+        color: "white",
+        borderColor: "white",
+        backgroundColor: "rgba(255, 255, 255, 0.08)",
+    },
+});
+
 const Profile = () => {
     return (
         <div className="profile-page-wrapper">
             <div className="profile-page-wrapper__block">
                 <h3>Password</h3>
-                <div className="profile-page-wrapper__block-content">Content</div>
+                <div className="profile-page-wrapper__block-content">
+                    <ProfileEditor type="number" label="Current password" />
+                    <ProfileEditor type="number" label="New password" />
+                    <ProfileEditor type="number" label="Confirm new password" />
+                    <ButtonProfile variant="outlined">Set</ButtonProfile>
+                </div>
             </div>
             <div className="profile-page-wrapper__block">
                 <h3>Account details</h3>
-                <div className="profile-page-wrapper__block-content"></div>
+                <div className="profile-page-wrapper__block-content">
+                    <ProfileEditor type="email" label="Email" />
+                    <ButtonProfile variant="outlined">Set</ButtonProfile>
+                </div>
             </div>
         </div>
     );
