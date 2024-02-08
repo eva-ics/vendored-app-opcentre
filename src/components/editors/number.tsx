72fb2b126a398e127dec2d0797560c34e1fe2ba8
Serhij S <div@altertech.com>
2024-02-08 23:44:24 +0100

    formula in trends

--- src/components/editors/number.tsx
+++ src/components/editors/number.tsx
@@ -4,60 +4,56 @@ import { THEME } from "../../common.tsx";
 import { parseNumber } from "bmat/numbers";
 
 const ErrorBar = ({ message }: { message: string | null }) => {
-  return <div className="form-error">{message}</div>;
+    return <div className="form-error">{message}</div>;
 };
 
 export const EditNumber = ({
-  element_id,
-  update_key,
-  current_value,
-  setParam,
-  params,
-  size
+    element_id,
+    update_key,
+    current_value,
+    setParam,
+    params,
+    size,
 }: {
-  element_id: string;
-  update_key?: any;
-  current_value: number;
-  setParam: (a: any) => void;
-  params?: any;
-  size?: boolean;
+    element_id: string;
+    update_key?: any;
+    current_value: number;
+    setParam: (a: any) => void;
+    params?: any;
+    size?: boolean;
 }): JSX.Element => {
-  const [input_value, setInputValue] = useState(
-    current_value === undefined ? "" : current_value.toString()
-  );
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
-  const currentInput = useRef<HTMLInputElement | null>(null);
+    const [input_value, setInputValue] = useState(
+        current_value === undefined ? "" : current_value.toString()
+    );
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
+    const currentInput = useRef<HTMLInputElement | null>(null);
 
-  useEffect(() => {
-    setInputValue(current_value === undefined ? "" : current_value.toString());
-  }, [element_id, update_key]);
+    useEffect(() => {
+        setInputValue(current_value === undefined ? "" : current_value.toString());
+    }, [element_id, update_key]);
 
-  return (
-    <div>
-      <ThemeProvider theme={THEME}>
-        <TextField
-          ref={currentInput}
-          type="number"
-          value={input_value}
-          variant="standard"
-          onChange={(e) => {
-            try {
-              const value = e.target.value;
-              setInputValue(value);
-              setParam(parseNumber(value, params));
-              if (setErrorMessage) {
-                setErrorMessage(null);
-              }
-            } catch (e: any) {
-              if (setErrorMessage) {
-                setErrorMessage(e.toString());
-              }
-            }
-          }}
-          sx={{ width: size ? "auto" : "40px" }}
-        />
-      </ThemeProvider>
-      {errorMessage && <ErrorBar message={errorMessage} />}
-    </div>
-  );
+    return (
+        <div>
+            <ThemeProvider theme={THEME}>
+                <TextField
+                    ref={currentInput}
+                    type="number"
+                    value={input_value}
+                    variant="standard"
+                    onChange={(e) => {
+                        try {
+                            const value = e.target.value;
+                            setInputValue(value);
+                            setParam(parseNumber(value, params));
+                            setErrorMessage(null);
+                        } catch (e: any) {
+                            setErrorMessage(e.toString());
+                        }
+                    }}
+                    sx={{ width: size ? "auto" : "40px" }}
+                />
+            </ThemeProvider>
+            {errorMessage && <ErrorBar message={errorMessage} />}
+        </div>
+    );
 };
