fd2b2e0f51d8b958a126bc1449c0ff1c01066bfc
Tasha Natasha <0955712824n@gmail.com>
2024-09-10 14:30:55 +0300

    fix click

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { GiHamburgerMenu } from "react-icons/gi";
-import { HeaderProps } from "../types";
+import { HeaderProps, NavElement } from "../types";
 import { Eva } from "@eva-ics/webengine";
 import { get_engine } from "@eva-ics/webengine-react";
 import { NavLink } from "react-router-dom";
@@ -32,12 +32,61 @@ const Header = ({ toggleMenu, nav, logout, current_page }: HeaderProps) => {
 
     const eva = get_engine() as Eva;
 
-    const handleSubClick = (event: any) => {
-        if (event.target.tagName !== "A") {
-            const link = event.currentTarget.querySelector("a");
-            if (link) {
-                link.click();
+    // const handleSubClick = (event: any) => {
+    //     if (event.target.tagName !== "A") {
+    //         const link = event.currentTarget.querySelector("a");
+    //         if (link) {
+    //             link.click();
+    //         }
+    //     }
+    // };
+    // Handle  clicks submenu
+
+    const handleSubClick = (
+        event:
+            | React.MouseEvent<HTMLAnchorElement, MouseEvent>
+            | React.KeyboardEvent<HTMLAnchorElement>,
+        to: string
+    ) => {
+        if (to === "logout") {
+            logout();
+        } else if (to.startsWith("/")) {
+            document.location = to;
+        }
+    };
+
+    // const handleSubKeyDown = (
+    //     event: React.KeyboardEvent<HTMLAnchorElement>,
+    //     to: string
+    // ) => {
+    //     event.preventDefault();
+
+    //     if (event.key === "Enter" || event.key === " ") {
+    //         if (to === "logout") {
+    //             logout();
+    //         } else if (to.startsWith("/")) {
+    //             document.location.href = to;
+    //         }
+    //     } else if (event.key === "Escape" || event.key === "Backspace") {
+    //         setOpenSubMenu(null);
+    //     }
+    // };
+
+    // Handle  clicks
+    const handleNavClick = (
+        event: React.MouseEvent<HTMLLIElement, MouseEvent>,
+        v: NavElement,
+        openSubMenu: string | null,
+        setOpenSubMenu: (value: string | null) => void
+    ) => {
+        if (v.submenus && v.submenus.length > 0) {
+            if (openSubMenu !== v.value) {
+                setOpenSubMenu(v.value);
+            } else {
+                setOpenSubMenu(null);
             }
+        } else if (v.to?.startsWith("/")) {
+            window.location.href = v.to;
         }
     };
 
@@ -73,26 +122,10 @@ const Header = ({ toggleMenu, nav, logout, current_page }: HeaderProps) => {
                             <li
                                 className={navLinkClass}
                                 key={idx}
-                                onClick={(event) => {
-                                    if (event.shiftKey) {
-                                        if (v.to?.startsWith("/")) {
-                                            window.open(v.to, "_blank");
-                                        }
-                                    } else {
-                                        if (
-                                            v.submenus &&
-                                            v.submenus.length > 0 &&
-                                            openSubMenu !== v.value
-                                        ) {
-                                            setOpenSubMenu(v.value);
-                                        } else {
-                                            setOpenSubMenu(null);
-                                            if (v.to?.startsWith("/")) {
-                                                window.location.href = v.to;
-                                            }
-                                        }
-                                    }
-                                }}
+                                tabIndex={idx}
+                                onClick={(event) =>
+                                    handleNavClick(event, v, openSubMenu, setOpenSubMenu)
+                                }
                             >
                                 {v.to ? (
                                     <NavLink key={idx} to={v.to}>
@@ -107,32 +140,32 @@ const Header = ({ toggleMenu, nav, logout, current_page }: HeaderProps) => {
                                     v.submenus.length > 0 && (
                                         <ul className="submenu">
                                             {v.submenus.map((submenuItem, subIdx) => (
-                                                <li
-                                                    className="submenu-item"
-                                                    key={subIdx}
-                                                    onClick={handleSubClick}
-                                                >
+                                                <li className="submenu-item" key={subIdx}>
                                                     <NavLink
                                                         to={
                                                             submenuItem.to === "logout"
                                                                 ? "?"
                                                                 : submenuItem.to
                                                         }
-                                                        onClick={() => {
-                                                            if (
-                                                                submenuItem.to.startsWith(
-                                                                    "/"
-                                                                )
-                                                            ) {
-                                                                document.location =
-                                                                    submenuItem.to;
-                                                            } else if (
-                                                                submenuItem.to ===
-                                                                "logout"
-                                                            ) {
-                                                                logout();
-                                                            }
-                                                        }}
+                                                        onClick={(event) =>
+                                                            handleSubClick(
+                                                                event,
+                                                                submenuItem.to
+                                                            )
+                                                        }
+                                                        tabIndex={0}
+                                                        // onKeyDown={(event) =>
+                                                        //     handleSubKeyDown(
+                                                        //         event,
+                                                        //         submenuItem.to
+                                                        //     )
+                                                        // }
+                                                        // onKeyDown={(event) =>
+                                                        //     handleSubClick(
+                                                        //         event,
+                                                        //         submenuItem.to
+                                                        //     )
+                                                        // }
                                                     >
                                                         {submenuItem.value}
                                                     </NavLink>
