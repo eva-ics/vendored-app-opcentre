3916be75c0b46511a9bbabdf2b4f98a08e6d264f
Tasha Natasha <0955712824n@gmail.com>
2024-12-20 19:35:26 +0200

    update scss

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "",
-  plugins: [react()],
-  css: {
-    devSourcemap: true,
-  },
+    base: "",
+    plugins: [react()],
+    css: {
+        devSourcemap: true,
+    },
+    resolve: {
+        alias: {
+            "@sass": "/src/sass",
+        },
+    },
 });
